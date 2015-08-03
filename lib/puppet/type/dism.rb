95fb4ad45273c448290fc5547d9e6adcdd3de6e0
Patrick Kelso <psk@puppetlabs.com>
2015-08-03 11:29:18 +1000

    Fix ensurable so it works with PE 2015.2, tested as working on 3.8 as well

--- lib/puppet/type/dism.rb
+++ lib/puppet/type/dism.rb
@@ -1,19 +1,7 @@
 Puppet::Type.newtype(:dism) do
   @doc = 'Manages Windows features via dism.'
 
-  ensurable do
-    desc 'Windows feature install state.'
-
-    defaultvalues
-
-    newvalue(:present) do
-      provider.create
-    end
-
-    newvalue(:absent) do
-      provider.destroy
-    end
-  end
+  ensurable
 
   newparam(:name, :namevar => true) do
     desc 'The Windows feature name (case-sensitive).'
